96cfc42775d9399d6dee3fa6877273a124bf8f38
raphaeldesosa <des22005@byui.edu>
2025-06-02 22:03:36 +0800

    completed learning activity 1 and 2

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
+
+router.get("/",utilities.handleErrors(accountController.buildAccount))
+router.get("/login",utilities.handleErrors(accountController.buildLogin))
+router.get("/register",utilities.handleErrors(accountController.buildRegister))
+router.post(
+    "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,11 +9,40 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const bodyParser = require("body-parser")
 const static = require("./routes/static")
-const baseContoller = require("./controllers/baseController")
+const baseController = require("./controllers/baseController")
 const utilities = require("./utilities")
 const inventoryRoute = require("./routes/inventoryRoute")
 const errorRoute = require("./routes/errorRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+
+
+/********************
+ * Middleware
+ *******************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and templates
@@ -29,9 +58,11 @@ app.set("layout", "./layouts/layout") // not at view roots
 app.use(static)
 app.use("/inv", inventoryRoute)
 app.use("/error", errorRoute)
+app.use("/account", accountRoute)
+
 
 // Index Route
-app.get("/", utilities.handleErrors(baseContoller.buildHome))
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
